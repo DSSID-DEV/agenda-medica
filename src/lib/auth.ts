acedfa71a4ac6371ff161e09738707524d6ebf80
DSSID-DEV <dssid.dev@gmail.com>
2025-05-30 00:46:49 -0300

    fix: handle optional clinic data in user session and improve redirection logic

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -27,15 +27,17 @@ export const auth = betterAuth({
       });
 
       // TODO: Ao adaptar para o usuário ter multiplas clinicas deverá ser retornado todas as clínicas do usuário.
-      const clinic = clinics[0];
+      const clinic = clinics?.[0];
 
       return {
         user: {
           ...user,
-          clinic: {
-            id: clinic.clinicId,
-            name: clinic.clinic.name,
-          },
+          clinic: clinic
+            ? {
+                id: clinic?.clinicId,
+                name: clinic?.clinic?.name,
+              }
+            : undefined,
         },
         session,
       };
